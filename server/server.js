43444142aa1c681a75165954f1e5c6e9261f9782
Luke Becker <beckerluke1@gmail.com>
2019-08-07 12:18:08 -0500

    Added responses for server to send back for /train, /first-train, /last-train, /count, and /random paths

---
+++ server/server.js
@@ -0,0 +1,44 @@
+const trains = [
+    { name: 'Thomas', color: 'Blue' },
+    { name: 'Gordon', color: 'Blue' },
+    { name: 'Henry', color: 'Green' },
+    { name: 'James', color: 'Red' }
+];
+
+const express = require('express'); 
+
+const app = express(); 
+const port = 5000; 
+
+app.use(express.static('server/public'));
+
+// Start up our server
+app.listen(port, () => {
+  console.log('listening on port', port);
+});
+
+app.get('/train', (req,res) => {
+    res.send(trains); 
+}); 
+
+app.get('/first-train', (req,res) => {
+    res.send(trains[0]); 
+}); 
+
+app.get('/last-train', (req,res) => {
+    res.send(trains[3]);
+});
+
+// counts number of trains and returns the object
+app.get('/count', (req,res) => {
+    let numberOfTrains = trains.length; 
+    const numberOfTrainsObj = { 
+        totalCount: numberOfTrains
+    };
+    res.send(numberOfTrainsObj); 
+});
+
+app.get('/random', (req,res) => {
+    let rand = trains[Math.floor(Math.random() * trains.length)];
+    res.send(rand); 
+}); 
